bdcec1ad7958eb2fa54a47a0ba7e61f045a395a0
Riley Bergamasco <rlbergamasco@Rileys-MacBook-Air-3.local>
2020-12-06 00:21:23 -0500

    Basic functionality and information

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
-import Question from './Question.js';
+import Popup from './Popup.js';
 
 class QuestionArr {
     /**question: String of question
      * answers: array containing the four answer choices
      * correctAnswer: int (0-3) of which answer is correct
      */
-
     constructor(question, answer0, answer1, answer2, answer3, correct){
       this.question = question;
       this.answer0 = answer0;
@@ -18,9 +17,11 @@ class QuestionArr {
     }
   }
   
+
+
   const questions = Array(10);
-  questions[0] = new QuestionArr("How much does an ambulance cost in the US?","$10","$50", "$1000", "$2500", 3);
-  questions[1] = new QuestionArr("How much does an ambulance cost in the UK or Canada??","$10","$50", "$1000", "$2500", 3);
+  questions[0] = new QuestionArr("How much does an ambulance cost in the US?","$45","$450", "$1000", "$2300", 3);
+  questions[1] = new QuestionArr("How much does an ambulance cost in Canada?","$45","$450", "$1000", "$2300", 1);
   questions[2] = new QuestionArr("How much does the average American spend on prescription and non-prescription medications annually?", "$75", "$500", "$1200", "$1500", 3);
   questions[3] = new QuestionArr("How much does the average UK resident spend on prescription and non-prescription medications annually?","$75", "$500", "$1200", "$1500", 2);
   questions[4] = new QuestionArr("What is the average cost of one EpiPen in the US?","$150", "$300", "$500", "$700", 4);
@@ -36,40 +37,94 @@ function App() {
     /**
      * qNum is the question number we are on
      */
-    const [qNum, setQNum] = useState(0);
+    let [qNum, setQNum] = useState(0);
+    let [state, setState] = useState(false);
 
     /**
      * Hook to increment qNum by 1,
      * changing the question to the next question
      */
     const nextQ = () => {
-        setQNum(qNum + 1);
+        if (qNum === 7) {
+            document.getElementById('conclusion').scrollIntoView();
+        } else {
+            document.getElementById('question').scrollIntoView();
+            setQNum(qNum + 1);
+        }
+    };
+
+    const previousQ = () => {
+        if (qNum === 0) {
+            document.getElementById('intro').scrollIntoView();
+        } if (qNum > 0) {
+            document.getElementById('question').scrollIntoView();
+            setQNum(qNum - 1);
+        }
+    };
+    
+
+    // let state = {
+    //     seen: false
+    // };
+    
+
+    const togglePop = () => {
+        setState(!state);
     };
 
     /**
      * should send popup when an answer is clicked
      */
     function popAnswer(answer) {
-        // console.log("hello");
-        if(answer == questions[qNum].correctAnswer){
+         setState(true);
+         
+        if(answer === questions[qNum].correctAnswer){
             //correct answer chosen
-            alert('correct!');
+            alert("That's right!");
         } else {
             //incorrect answer chosen
-            alert('incorrect');
+            alert("Try again!");
         }
     };
-    
-    return (
-        <div>
-            <Question  text={questions[qNum].question} 
-            answer1={questions[qNum].answer0}
-            answer2={questions[qNum].answer1}
-            answer3={questions[qNum].answer2}
-            answer4={questions[qNum].answer3}
-            popAnswer={popAnswer()}
-            />
 
+    return (        
+        <div className="slides">
+            <div id="intro">
+                <h1>US Healthcare:</h1>
+                <h2>What it really costs</h2>
+                <button id="start" onClick={()=>document.getElementById('question').scrollIntoView()}>START</button>
+            </div>
+            <div id="question">
+                
+                <h2>{questions[qNum].question}</h2>
+                <section className="grid-4">
+                <button onClick={()=>popAnswer(1)}> {questions[qNum].answer0}</button>
+                <button onClick={()=>popAnswer(2)}> {questions[qNum].answer1}</button>
+                <button onClick={()=>popAnswer(3)}> {questions[qNum].answer2}</button>
+                <button onClick={()=>popAnswer(4)}> {questions[qNum].answer3}</button>
+                <button class="change" onClick={previousQ}>Back</button>
+                <button class="change" onClick={nextQ}>Next</button>
+                <Popup pop={popAnswer}/>
+                <div>
+                {/* <div className="btn" onClick={this.togglePop}>
+                <button>New User?</button>
+                </div> */}
+                {state ? <Popup toggle={togglePop} /> : null}
+                </div>
+                </section>
+            </div>
+            <div id="conclusion">
+                <h1>Conclusion</h1>
+                <h3>For more information, check out these links:</h3>
+                <ul>
+                    <li><a href="https://www.forbes.com/sites/nextavenue/2019/10/25/what-broke-american-health-care-and-how-to-fix-it/?sh=57aebd5147ed">What Broke American Health Care And How To Fix It</a> 
+                    </li>
+                    <li><a href="https://www.pgpf.org/blog/2020/07/how-does-the-us-healthcare-system-compare-to-other-countries">How Does The U.S. Healthcare System Compare To Other Countries?</a></li>
+                    
+                </ul>
+                
+            </div>
+            
         </div>
     )
 }
--- my-app/src/Popup.js
+++ my-app/src/Popup.js
@@ -6,14 +6,16 @@ function Popup() {
         document.getElementById("pop").style.display = "block";
     };
 
+    
+
     const [displayType, setDisplay] = useState("none");
+    const [text, setText] = useState("Try again!")
 
     return (
         <div id="pop" className="modal">
           <span className="close">&times;</span>
           <div className="modal-content">
             <p>That's right!</p>
-            <button>NEXT</button>
           </div>
         </div>
     )
